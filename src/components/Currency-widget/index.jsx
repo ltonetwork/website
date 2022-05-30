7b76533b2a3bba61908ff3a4ea416265d292780e
Joshua van Boxtel <joshua.boxtel@crowdlinker.com>
2022-05-29 23:24:05 -0400

    Nav - ensure currency widget remains in place between page transitions

--- src/components/Currency-widget/index.jsx
+++ src/components/Currency-widget/index.jsx
@@ -1,10 +1,14 @@
 import Head from "next/head";
-import Script from 'next/script'
 
 const CurrencyWidget = () => {
+  setTimeout(function(){ 
+    var script = document.createElement('script');
+    script.src = 'https://files.coinmarketcap.com/static/widget/currency.js';
+    var head = document.getElementsByTagName("head")[0];
+    head.appendChild(script);
+  })
   return (
     <>
-      <Script type="text/javascript" src="https://files.coinmarketcap.com/static/widget/currency.js"></Script>
       {//@ts-ignore
         <div className="currency-widget coinmarketcap-currency-widget" data-currencyid="3714" data-base="USD" data-secondary="" data-ticker="true" data-rank="true" data-marketcap="true" data-volume="true" data-statsticker="true" data-stats="USD"></div>}
     </>
