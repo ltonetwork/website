f047cb68d40784eee15b404ff2f862bb6cc0f9df
itsnaderi <105480063+itsnaderi@users.noreply.github.com>
2022-05-30 16:29:25 +1000

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -18,18 +18,7 @@ class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png" />
 
       
- <meta property="og:title" content="LTO Network - Start Your Journey">
-<meta property="og:type" content="website">
 
-<meta property="og:image" content="https://www.ltonetwork.com/img/logo-light.svg">
-<meta property="og:url" content="https://ltonetwork.com">
-<meta property="og:description" content="Next-Gen blockchain for B2B, Identities, Privacy, Ownership and Digital Collectibles.">
-
-<meta name="twitter:image" content="https://www.ltonetwork.com/img/logo-light.svg">
-<meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:title" content="LTO Network - Start Your Journey">
-<meta name="twitter:description" content="Next-Gen blockchain for B2B, Identities, Privacy, Ownership and Digital Collectibles.">
-      
       
           {/* Google Fonts */}
           <link
