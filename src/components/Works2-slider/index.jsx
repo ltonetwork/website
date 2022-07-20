4ff396cf8ecab3845eefe3559720aeb898e726a5
itsnaderi <105480063+itsnaderi@users.noreply.github.com>
2022-07-20 19:12:36 +1000

    Update index.jsx

--- src/components/Works2-slider/index.jsx
+++ src/components/Works2-slider/index.jsx
@@ -151,6 +151,7 @@ const Works2Slider = ({ subBG }) => {
                           data-wow-delay=".3s"
                         >
                         
+                          <div className="top-label">Q3 - 2022</div>
                          
                         <div
                             className="item-img bg-img wow imago"
@@ -191,7 +192,6 @@ const Works2Slider = ({ subBG }) => {
                         >
                         
                         
-                          <div className="top-label">Q3 - 2022</div>
                           <div
                             className="item-img bg-img wow imago"
                             style={{
