e9005a484ab90aae22bfaba3768276505cabd25d
itsnaderi <105480063+itsnaderi@users.noreply.github.com>
2023-10-12 20:09:32 +1100

    Update index.jsx

--- src/components/Home2023/TrueAssetOwnership/index.jsx
+++ src/components/Home2023/TrueAssetOwnership/index.jsx
@@ -8,7 +8,7 @@ const TrueAssetOwnership = () => {
           <div className="container">
             <h2 className="title-2023 mb-20">True Asset Ownership</h2>
             <p className="mb-40">Our network is built for Ownership.</p>
-            <p>Using our <span style={{ color:"#00FFFF" }}>hybrid architecture</span> we have developed a world class GDPR compliant & Privacy aware suite of applications and an ecosystem that bring true decentralized ownership to the digital space.</p>
+            <p>Using our <span style={{ color:"#00FFFF" }}>hybrid architecture</span>, we have developed a world-class GDPR-compliant and privacy-aware suite of applications and an ecosystem that bring true decentralized ownership for real-world assets to the digital space.</p>
             <img src="/img/home-2023/tao-graphic-v2.png" alt="Private Layer consists of Identity, Assets, and Tokenization while Public Layer consists of Secure and Verify" />
             <Link href="https://blog.ltonetwork.com/lto-network-in-a-nutshell/" target="_blank">
               <a className="link-2023 mt-50">Learn More</a>
