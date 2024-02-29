2efc4547d73a4d38cff472335641a2141ae0fc31
colStie <stiemsdev@gmail.com>
2024-02-29 09:35:49 +0100

    comment out line

--- src/components/Home2023/TrueAssetOwnership/index.jsx
+++ src/components/Home2023/TrueAssetOwnership/index.jsx
@@ -8,7 +8,7 @@ const TrueAssetOwnership = () => {
           <div className="container">
             <h2 className="title-2023 mb-20">True RWA Ownership</h2>
             <p className="mb-40">Our network is built for Ownership.</p>
-           <!-- <p>Using our <span style={{ color:"#00FFFF" }}>hybrid architecture</span>, we have developed a world-class GDPR-compliant and privacy-aware suite of applications and an ecosystem that bring true decentralized ownership for real-world assets to the digital space.</p>-->
+           {/* <!-- <p>Using our <span style={{ color:"#00FFFF" }}>hybrid architecture</span>, we have developed a world-class GDPR-compliant and privacy-aware suite of applications and an ecosystem that bring true decentralized ownership for real-world assets to the digital space.</p>--> */}
                
            <p>Our <span style={{ color:"#00FFFF" }}>2024</span> RWA Roadmap is here. We have revealed the world's most advanced ecosystem for RWAs, bringing true decentralized ownership for real-world assets to the digital space.</p>
             <img src="/img/home-2023/tao-graphic-v2.png" alt="Private Layer consists of Identity, Assets, and Tokenization while Public Layer consists of Secure and Verify"/>
