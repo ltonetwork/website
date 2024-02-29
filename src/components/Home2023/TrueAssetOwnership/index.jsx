d6509a30a3524d98cc7168adf2dbe005d3faf894
colStie <stiemsdev@gmail.com>
2024-02-29 10:53:36 +0100

    &apos; change

--- src/components/Home2023/TrueAssetOwnership/index.jsx
+++ src/components/Home2023/TrueAssetOwnership/index.jsx
@@ -10,7 +10,7 @@ const TrueAssetOwnership = () => {
             <p className="mb-40">Our network is built for Ownership.</p>
            {/* <!-- <p>Using our <span style={{ color:"#00FFFF" }}>hybrid architecture</span>, we have developed a world-class GDPR-compliant and privacy-aware suite of applications and an ecosystem that bring true decentralized ownership for real-world assets to the digital space.</p>--> */}
                
-           <p>Our <span style={{ color:"#00FFFF" }}>2024</span> RWA Roadmap is here. We have revealed the world's most advanced ecosystem for RWAs, bringing true decentralized ownership for real-world assets to the digital space.</p>
+           <p>Our <span style={{ color:"#00FFFF" }}>2024</span> RWA Roadmap is here. We have revealed the world&apos;s most advanced ecosystem for RWAs, bringing true decentralized ownership for real-world assets to the digital space.</p>
             <img src="/img/home-2023/tao-graphic-v2.png" alt="Private Layer consists of Identity, Assets, and Tokenization while Public Layer consists of Secure and Verify"/>
             <Link href="https://blog.ltonetwork.com/rwa_roadmap/" target="_blank">
               <a className="link-2023 mt-50">2024 RWA ROADMAP</a>
