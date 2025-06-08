28c31168369cbe880c4774d45303ebc6f511c968
colStie <stiemsdev@gmail.com>
2025-06-08 16:42:57 +0200

    fix(hero): improve mobile visibility of down arrow and finalize hero clean-up

--- src/common/loadingPace.js
+++ src/common/loadingPace.js
@@ -8,7 +8,7 @@ const loadingPace = () => {
   Pace.on("done", function () {
     //When Pace End
     if (document.querySelector(".hamenu")) {
-      delay(300, animateEl(document.querySelector(".hamenu"), "-100%"));
+      delay(75, animateEl(document.querySelector(".hamenu"), "-100%"));
       document.querySelector(".topnav .menu-icon").classList.remove("open");
     }
     document.querySelector("#preloader").classList.add("isdone");
--- src/components/Navbar-full-menu/navbar-full-menu/index.jsx
+++ src/components/Navbar-full-menu/navbar-full-menu/index.jsx
@@ -17,31 +17,45 @@ const NavbarFullMenu = ({ theme, lr }) => {
         className={`topnav ${theme ? (theme === "light" ? "light" : "") : ""}`}
       >
         <div className="container-fluid">
-          <div className="logo">
-            <Link href="/">
-              <a>
-                {theme ? (
-                  theme === "light" ? (
-                    <img src={`${appData.darkLogo}`} alt="logo" />
-                  ) : (
-                    <img src={`${appData.lightLogo}`} alt="logo" />
-                  )
-                ) : (
-                  <img src={`${appData.lightLogo}`} alt="logo" />
-                )}
+          <div className="navbar-header-block">
+            <div className="navbar-header-block-row">
+              <div className="navbar-logo flex-seq logo-seq" style={{ order: 1 }}>
+                <Link href="/">
+                  <a>
+                    {theme ? (
+                      theme === "light" ? (
+                        <img src={`${appData.darkLogo}`} alt="logo" />
+                      ) : (
+                        <img src={`${appData.lightLogo}`} alt="logo" />
+                      )
+                    ) : (
+                      <img src={`${appData.lightLogo}`} alt="logo" />
+                    )}
+                  </a>
+                </Link>
+              </div>
+              <div className="menu-icon flex-seq menu-seq" style={{ order: 3 }}>
+                <span className="icon">
+                  <i></i>
+                  <i></i>
+                </span>
+                <Split>
+                  <span className="text" data-splitting style={{ paddingTop: '4px' }}>
+                    <span className="menu-text">Menu</span>
+                  </span>
+                </Split>
+              </div>
+            </div>
+            <div className="navbar-download-center flex-seq download-seq" style={{ order: 2 }}>
+              <a href="https://apps.apple.com/app/lto-wallet/id6447391863" target="_blank" rel="noopener noreferrer" className="btn-download btn-app-store">
+                <i className="fab fa-apple me-2"></i>
+                App Store
               </a>
-            </Link>
-          </div>
-          <div className="menu-icon">
-            <span className="icon">
-              <i></i>
-              <i></i>
-            </span>
-            <Split>
-              <span className="text" data-splitting style={{ paddingTop: '4px' }}>
-                <span className="menu-text">Menu</span>
-              </span>
-            </Split>
+              <a href="https://play.google.com/store/apps/details?id=com.ltonetwork.wallet" target="_blank" rel="noopener noreferrer" className="btn-download btn-play-store">
+                <i className="fab fa-google-play me-2"></i>
+                Play Store
+              </a>
+            </div>
           </div>
         </div>
       </div>
